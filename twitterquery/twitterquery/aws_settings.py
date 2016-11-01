49a2403b7b72100c6c6c0f41f42ca4cb0b2fd89c
SAM Creative <samcreative@SAMCreative.local>
2016-11-01 15:34:48 -0700

    Updated aws settings for enviorn database passes

--- twitterquery/twitterquery/aws_settings.py
+++ twitterquery/twitterquery/aws_settings.py
@@ -4,6 +4,8 @@ Django aws settings for twitterquery project.
 
 from .shared_settings import *
 
+import os
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -12,13 +14,14 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'query',
-        'USER': 'twitterquery_db',
-        'PASSWORD': '2299>>snared',
-        'HOST': 'twitterquery.cpchpntjumj7.us-west.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+if 'RDS_HOSTNAME' in os.environ:
+	DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	        'NAME': os.environ['RDS_DB_NAME'],
+	        'USER': os.environ['RDS_USERNAME'],
+	        'PASSWORD': os.environ['RDS_PASSWORD'],
+	        'HOST': os.environ['RDS_HOSTNAME'],
+	        'PORT': os.environ['RDS_PORT'],
+	    }
+	}
