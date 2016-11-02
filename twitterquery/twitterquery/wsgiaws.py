8d2fc8b3bd3ffe42d2cbd29fb884941fb4b06b0f
SAM Creative <samcreative@SAMCreative.local>
2016-11-01 18:10:09 -0700

    Almost there

--- twitterquery/twitterquery/wsgiaws.py
+++ twitterquery/twitterquery/wsgiaws.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitterquery.prod_settings")
+sys.path.insert(0, '/opt/python/current/app/twitterquery')
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitterquery.aws_settings")
 
 application = get_wsgi_application()
